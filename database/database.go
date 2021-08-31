807b6fa2945b80e6d76708aa39f27e8ddd02d687
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2021-08-31 15:03:46 -0700

    Deploy

--- database/database.go
+++ database/database.go
@@ -2,9 +2,7 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"strconv"
 
 	"github.com/asmusj224/exercise-automation/config"
 
@@ -14,28 +12,15 @@ import (
 var DB *sql.DB
 
 func Connect() error {
-	port, err := ParseDatabasePort()
-	if err != nil {
-		log.Println("Error parsing database port")
-		return err
+	database_url := config.Config("DATABASE_URL")
+
+	if database_url == "" {
+		log.Fatal("$DATABASE_URL must be set")
 	}
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME")))
+	db, err := sql.Open("postgres", database_url)
 	if err != nil {
 		return err
 	}
 	DB = db
 	return nil
-}
-
-func ParseDatabasePort() (uint64, error) {
-	var err error
-	p := config.Config("DB_PORT")
-
-	port, err := strconv.ParseUint(p, 10, 32)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return port, nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
+	"github.com/asmusj224/exercise-automation/config"
 	"github.com/asmusj224/exercise-automation/controller"
 	"github.com/asmusj224/exercise-automation/database"
 	"github.com/asmusj224/exercise-automation/services"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func main() {
+
+	port := config.Config("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	app := gin.Default()
 
 	err := database.Connect()
@@ -59,15 +66,7 @@ func main() {
 	app.GET("/api/v1/exercise-workout/:id", exerciseWorkout.GetExerciseWorkoutByID)
 	app.PUT("/api/v1/exercise-workout/:id", exerciseWorkout.UpdateExerciseWorkoutByID)
 
-	app.GET("/test", func(c *gin.Context) {
-		id, _ := uuid.Parse("cf89c692-050d-448c-ae57-4ba3bb4ee518")
-		found, _ := store.GetExerciseWorkoutByWorkoutId(c, id)
-		c.JSON(http.StatusOK, gin.H{
-			"result": found,
-		})
-	})
-
 	defer database.DB.Close()
 
-	app.Run(":3000")
+	app.Run(":" + port)
 }
